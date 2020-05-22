7a28091ebc85701510978929b3cfd017bc53bc92
Alex S <alextheprogrammer21@gmail.com>
2020-05-22 16:17:25 -0700

    Added a send_mail function

--- remote/execute-command.py
+++ remote/execute-command.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess, smtplib
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
 
 command = "echo test"
 
