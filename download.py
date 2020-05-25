018a097c1c2aa4e6877a0d1a01321c6a49a2d387
Alex S <alextheprogrammer21@gmail.com>
2020-05-24 19:42:02 -0700

    Saves file to disc

--- download.py
+++ download.py
@@ -4,6 +4,8 @@ import requests
 
 def download(url): 
     get_response = requests.get(url)
-    print(get_response.content)
+    file_name = url.split("/")[-1]
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
 
 download("https://www.tibco.com/blog/wp-content/uploads/2020/01/TIBCOandPython-1-scaled-e1579201642399.jpg")
